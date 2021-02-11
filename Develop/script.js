48650a381c8e2908925fa7f126a1a1d155916a10
jdrabot <jdrabot@hotmail.com>
2021-02-10 22:20:08 -0500

    working on javascript file. naming functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,25 +1,36 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowercasechar = "abcdefghijklmnopqrstuvwxyz";
-var uppercasechar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numboxchar = 0812934567;
-var symboxchar = "~!@#$%^&*()_-+{}][;:]?/.><,";
+var lowercasechar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercasechar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numboxchar = ["0", "8", "1", "2", "9", "3", "4", "5", "6", "7"];
+var symboxchar = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "{", "}", "]", "[", ";", ":", "?", "/", ".", "<", ","];
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  
-
   passwordText.value = password;
-
 }
+function createPassword () {
+  var charLength = parseInt(prompt("How many characters do you want in your password?"));
+  if (charLength < 8 || charLength > 128) {
+    window.alert("Pick a number between 8 and 128 punk.");
+    createPassword();
+  } else {
+    var lcChoice = lowersChoice();
+    var ucChoice = uppersChoice();
+    var numChoice = numbersChoice();
+    var symChoice = symbolsChoice();
+  }
+}
+function lowersChoice () {
+  var lcChars = confirm("Do you want to use lowercase letters in your password?");
+  var lcChoice = lcChars;
+  return lcChoice;
+};
+function uppersChoice ();
+function numbersChoice ();
+function symbolsChoice ();
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-  window.prompt ("Do you want to include lowercase letters?");
-  window.prompt ("Do you want to include uppercase letters?");
-  window.prompt ("Do you want to include numbers?");
-  window.prompt ("Do you want to include symbols?");
